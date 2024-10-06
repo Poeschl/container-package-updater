eaa5d1a16ac2f0983858be0c98bb193a1401cbb0
Markus Pöschl <Poeschl@users.noreply.github.com>
2024-10-06 13:50:36 +0200

    :white_check_mark: Add Test in CI

--- containerpackageupdater/main.py
+++ containerpackageupdater/main.py
@@ -19,7 +19,7 @@ def write_containerfile(file_path: str, content: str):
 
 
 def update_single_version(package: Package, latest_package: Package, container_file: str, package_manager: PackageManagerHandler, container_file_content: str,
-                          push_repository: str, token: str):
+                          push_repository: str, token: str, dry_run: bool):
   logging.info(f'Package "{package.name}" is outdated. Current version: {package.version}, latest version: {latest_package.version}')
 
   reset_to_main_branch()
@@ -34,12 +34,16 @@ def update_single_version(package: Package, latest_package: Package, container_f
     pr_body = "| Package Name | Current Version | Latest Version |\n"
     pr_body += "|--------------|-----------------|----------------|\n"
     pr_body += f"| {package.name} | {package.version} | {latest_package.version} |\n"
-    create_pull_request(token, push_repository, branch_name, change_name, pr_body)
+    if not dry_run:
+      create_pull_request(token, push_repository, branch_name, change_name, pr_body)
+    else:
+      logging.info(f"Would have created a PR for {package.name} -> {latest_package.version}")
   except ValueError:
     logging.info('Skipping update for package. Already exists in a PR.')
 
 
-def main(token: str, container_file: str, push_repository: str, os_version: str, architectures: list[str]) -> int:
+def main(token: str, dry_run: bool, container_file: str, push_repository: str, os_version: str,
+         architectures: list[str]) -> int:
   reset_to_main_branch()
 
   container_file_content = read_containerfile(container_file)
@@ -62,7 +66,8 @@ def main(token: str, container_file: str, push_repository: str, os_version: str,
     if len(latest_packages) == 1:
       latest_package = latest_packages[0]
       if package.version != latest_package.version:
-        update_single_version(package, latest_package, container_file, package_manager, container_file_content, push_repository, token)
+        update_single_version(package, latest_package, container_file, package_manager, container_file_content,
+                              push_repository, token, dry_run)
       else:
         logging.info(f'Package "{package.name}" is up to date.')
     elif len(latest_packages) > 1:
@@ -83,7 +88,8 @@ if __name__ == '__main__':
   parser.add_argument('--repository', required=False, help='The repository update PRs should be created in', default=os.environ.get('GITHUB_REPOSITORY'))
   parser.add_argument('--osVersion', required=False, help='The os version to use for the version check. Example "3.18" for alpine', default=3.18)
   parser.add_argument('--architecture', required=False, help='The architectures to check. (Comma seperated in string)', default=['x86_64'], action='append')
+  parser.add_argument('--dryRun', required=False, help='If true, no PR is created.', default=False)
 
   args = parser.parse_args()
 
-  exit(main(args.token, args.containerFile, args.repository, args.osVersion, args.architecture))
+  exit(main(args.token, args.dryRun, args.containerFile, args.repository, args.osVersion, args.architecture))
