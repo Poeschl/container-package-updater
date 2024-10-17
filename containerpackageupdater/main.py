c1fb31af4660af93fc7ae18c4dbfceefa9dd2534
Markus Pöschl <Poeschl@users.noreply.github.com>
2024-10-17 12:23:25 +0200

    :loud_sound: Set logging level back to info

--- containerpackageupdater/main.py
+++ containerpackageupdater/main.py
@@ -99,7 +99,7 @@ def str2bool(v):
 
 
 if __name__ == '__main__':
-  logging.basicConfig(level=logging.DEBUG, format="%(asctime)s %(levelname)s [%(name)s] %(message)s", datefmt="%Y-%m-%d %H:%M:%S", stream=sys.stdout)
+  logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s [%(name)s] %(message)s", datefmt="%Y-%m-%d %H:%M:%S", stream=sys.stdout)
 
   parser = argparse.ArgumentParser(description='Docker Package Updater')
   parser.add_argument('--token', required=True, help='The GitHub token to use for PR creation')
